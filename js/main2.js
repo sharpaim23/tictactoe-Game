0728d6f872f807b6ef4285a4471efdeb9954fe6d
Aimee Sharp <sharpaim23@gmail.com>
2022-05-10 14:41:26 -0500

    Add more code to make JS even more OOP

--- js/main2.js
+++ js/main2.js
@@ -43,39 +43,67 @@ class TicTacToe{
     this.render()
     return true
   }
-
- 
-
-  render(){
+  restart(){
+    this._board = [0,0,0,0,0,0,0,0,0];
+    this._currentPlayer = 'X'
+    this.render()
+  }
+render(){
 
   }
 }
 
 class DOMTicTacToe extends TicTacToe{
-  constructor(currentPlayerElement, buttons){
+  constructor(currentPlayerElement,messageOutput,restartButton,buttons){
     super()
     this.currentPlayerElement = currentPlayerElement
+    this.messageOutput = messageOutput
+    this.restartButton = restartButton
     this.buttons = buttons
 
+    this.message = ''
+
     this.buttons.forEach((button, i) => button.addEventListener('click', this.place.bind(this, i)))
+    this.restartButton.addEventListener('click', this.restart.bind(this))
   }
 
- 
+  restart(){
+    this.message = ''
+    super.restart();
+  }
 
   render(){
+    super.render();
+
     for(let i = 0; i < this._board.length; i++){
-      if(!this._board[i]) continue;
-      this.buttons[i].textContent = this._board[i];
-      this.buttons[i].disabled = true;
+      if(this._board[i]) {
+
+        this.buttons[i].textContent = this._board[i];
+        this.buttons[i].disabled = true;
+      }else {
+
+        this.buttons[i].textContent = ''
+        this.buttons[i].disabled = false;
+      }    
     }
+
     this.currentPlayerElement.textContent = this.currentPlayer
+
     const winner = this.winner;
-      if(winner === undefined) return;
-      this.buttons.forEach(button => button.disabled = true)
-      alert(winner ? winner + ' Won!' : 'Draw!')
+      if(winner !== undefined){
+
+        this.buttons.forEach(button => button.disabled = true)
+        this.message = winner ? winner + ' Won!' : 'Draw!'
+      } 
+      this.messageOutput.innerHTML = this.message
   }
 }
-new DOMTicTacToe(document.querySelector('#current-player'),[...document.querySelectorAll('button')])
+
+new DOMTicTacToe(
+document.querySelector('#current-player'),
+document.querySelector('#message'),
+document.querySelector('footer button'),
+[...document.querySelectorAll('#board button')])
 
 
 
