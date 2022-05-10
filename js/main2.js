6cf3839d2f480007e68fc407e9d151460374f17d
Aimee Sharp <sharpaim23@gmail.com>
2022-05-10 14:02:50 -0500

    Make fully OOP

--- js/main2.js
+++ js/main2.js
@@ -26,6 +26,7 @@ class TicTacToe{
     if(value) return false;
     this._board[i] = this._currentPlayer
     this._currentPlayer = this._currentPlayer === 'X' ? 'O' : 'X'
+    this.render()
     return true
   }
 
@@ -42,25 +43,37 @@ class TicTacToe{
     }
     return undefined
   }
+
+  render(){
+
+  }
 }
 
+class DOMTicTacToe extends TicTacToe{
+  constructor(currentPlayerElement, buttons){
+    super()
+    this.currentPlayerElement = currentPlayerElement
+    this.buttons = buttons
 
-const tictactoe = new TicTacToe( )
-const buttons = [...document.querySelectorAll('button')]
-const currentPlayerElement = document.querySelector('#current-player')
+    this.buttons.forEach((button, i) => button.addEventListener('click', this.place.bind(this, i)))
+  }
 
-buttons.forEach((button, i) =>{
-  button.addEventListener('click', event => {
-    if(!tictactoe.place(i)) return;
+ 
 
-    event.currentTarget.textContent = tictactoe.currentPlayer;
-    event.currentTarget.disabled = true;
-    currentPlayerElement.textContent = tictactoe.currentPlayer
-    const winner = tictactoe.winner;
-    if(winner === undefined) return;
-    buttons.forEach(button => button.disabled = true)
-    alert(winner ? winner + ' Won!' : 'Draw!')
-    
-  })
-})
+  render(){
+    for(let i = 0; i < this._board.length; i++){
+      if(!this._board[i]) continue;
+      this.buttons[i].textContent = this._board[i];
+      this.buttons[i].disabled = true;
+    }
+    this.currentPlayerElement.textContent = this.currentPlayer
+    const winner = this.winner;
+      if(winner === undefined) return;
+      this.buttons.forEach(button => button.disabled = true)
+      alert(winner ? winner + ' Won!' : 'Draw!')
+  }
+}
+new DOMTicTacToe(document.querySelector('#current-player'),[...document.querySelectorAll('button')])
+
+
 
